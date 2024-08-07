06341f09a1051a5a5477ce8a8498a349acddba4f
Yousef Saad <99688907+yousefsaad12@users.noreply.github.com>
2024-08-07 09:52:38 +0100

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,7 +31,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="/me.png"
+              src="/me.jpg"
               alt="Yousef portrait"
               width="192"
               height="192"
