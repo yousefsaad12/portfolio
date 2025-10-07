e43f721389bd33230c33a6a2702491a59c1c4a43
Yousef Saad <saadrslan95@gmail.com>
2025-10-07 02:54:27 +0100

    some changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useRef } from "react";
 import { motion, useScroll, useTransform } from "framer-motion";
 import { FileText } from "lucide-react";
 
